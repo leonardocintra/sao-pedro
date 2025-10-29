70d000dd6c97386071d4f824e06ff20690e8f8de
Leonardo Cintra <leonardo.ncintra@outlook.com>
2025-10-29 13:12:20 -0300

    :sparkles: add filter by uuid

--- pessoas/filters.py
+++ pessoas/filters.py
@@ -6,4 +6,4 @@ from pessoas.models import Pessoa
 class PessoaFilter(django_filters.FilterSet):
     class Meta:
         model = Pessoa
-        fields = ["cpf"]  # Adicione outros campos conforme necessário
+        fields = ["cpf", "uuid"]  # Adicione outros campos conforme necessário
